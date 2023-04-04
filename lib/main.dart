0bb85f3f915d5dd3d25261191704c40566ce56d2
Saumya-Purohit <saumyanotes@gmail.com>
2023-04-04 14:07:45 +0530

    name of func not whole func to answer question instead of null

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,10 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer Chosen! ');
+  }
+
   @override
   Widget build(BuildContext context) {
     var questions = [
@@ -24,15 +28,15 @@ class MyApp extends StatelessWidget {
             Text('The question is '),
             ElevatedButton(
               child: Text('Answer 1'),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
             ElevatedButton(
               child: Text('Answer 2'),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
             ElevatedButton(
               child: Text('Answer 3'),
-              onPressed: null,
+              onPressed: answerQuestion,
             )
           ],
         ),
